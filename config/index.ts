59de864beeec52f8fd653e54165141f020f4696d
moneko97 <poi.nyaa@qq.com>
2025-09-05 01:07:16 +0800

    update

--- config/index.ts
+++ config/index.ts
@@ -1,14 +1,6 @@
 import type { ConfigType } from 'PackageNameByCore';
 
 const conf: Partial<ConfigType> = {
-  importOnDemand: {
-    '@moneko/common': {
-      transform: 'lib/${member}',
-    },
-    lodash: {
-      transform: '${member}',
-    },
-  },
   proxy: {
     '/api/': {
       target: 'http://127.0.0.1:8001/',
